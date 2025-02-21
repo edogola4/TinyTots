83208c14a7818b63aa78f2aaf5ed036caf5ef852
edogola4 <edogola4@gmail.com>
2025-02-21 15:21:13 +0300

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Navbar } from './Navbar.jsx';
 export { default as Main } from './main';
-export { default as Product } from './Product.jsx';
-export { default as Products } from './Product.jsx';
+export { default as Product } from './Products.jsx';
+export { default as Products } from './Products.jsx';
 export { default as Footer } from './Footer';
-export { default as Items } from './Items.json';     // Exports JSON data (if needed)
+//export { default as Items } from '../../public/Items.json';     // Exports JSON data (if needed)
